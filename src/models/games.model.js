29a8f2a6dbbafc4a626fd4fb722430ae1e60d12c
marz-be <marzban.be@gmail.com>
2017-08-01 15:23:24 +0200

    hooks added

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -1,14 +1,28 @@
 // games-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const playerSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    pairs: [String],
+  });
+
   const games = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    numbersToChoose: { type: String,  },
+    goalNumber: { type: String, },
+    players: [playerSchema],
+    started: { type: Boolean,  },
+    winnerId: { type: String, },
+    draw: { type: String,  },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
+
 
   return mongooseClient.model('games', games);
 };
